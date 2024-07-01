e1222b2741010fb85cf665db8398cecc3256e5eb
avogvrl <avogvrl@gmail.com>
2024-07-01 11:13:17 +1000

    initial commit

---
+++ spesificode/src/aiIntegration.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+
+
+async function getAIPoweredResponse(prompt: string): Promise<string>{
+    const apiUrl= "http://localhost:3000"
+
+    try{
+        const response = await axios.post(apiUrl, {prompt})
+        return response.data.bot.trim;
+    }
+    catch(error){
+        console.error("Error fetching response:", error)
+        return 'Something went wrong'
+    }
+}
+
+export { getAIPoweredResponse }
---
+++ spesificode/src/extension.ts
@@ -0,0 +1,70 @@
+import * as vscode from 'vscode'
+import { getAIPoweredResponse } from './aiIntegration'
+
+
+//Typing effect
+async function typeTextInEditor(editor: vscode.TextEditor, text: string){
+	for(let i=0; i<text.length; i++){
+		//adjust the delay of text
+		await new Promise(resolve =>setTimeout(resolve, 50))
+		editor.edit(editBuilder =>{
+			editBuilder.insert(editor.selection.active, text[i])
+		})
+	}
+}
+
+
+
+//Handle user input
+async function handleUserInput(){
+	const prompt = await vscode.window.showInputBox({
+		prompt: "Please enter your prompt"
+	})
+
+	//If user cancels the input
+	if(prompt === undefined){
+		return;
+	}
+
+	//Get active text editor
+	const editor = vscode.window.activeTextEditor
+
+	if(!editor){
+		return
+	}
+
+	//Display loading message
+	editor.edit( editBuilder =>{
+		editBuilder.insert(editor.selection.active, 'Fetching Response ...')
+	})
+
+	//Fetch bot response
+	const botResponse = await getAIPoweredResponse(prompt)
+
+	//Remove loading message
+	const loadingMessageLength = 'Fetching Response ...'.length
+
+	editor.edit(editBuilder=>{
+		editBuilder.delete(
+			new vscode.Range(
+				editor.selection.active.translate(0, -loadingMessageLength),
+				editor.selection.active
+			)
+		)
+	})
+
+
+	//Simulate typing affect for the bot response
+	await typeTextInEditor(editor, botResponse)
+
+	//Display completion
+	vscode.window.showInformationMessage('Response is received and typed')
+
+}
+
+export function active(context: vscode.ExtensionContext){
+	let disposable = vscode.commands.registerCommand('extension.getAIPoweredBotResponse', async()=>{
+		await handleUserInput()
+	})
+	context.subscriptions.push(disposable)
+}
